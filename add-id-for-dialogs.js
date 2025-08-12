23d43ac7e08f16b48965d71e8f64af1c808edffd
AmberJillard <amber@lexgillette.com>
2025-08-12 16:48:55 -0700

    Create add-id-for-dialogs.js

---
+++ add-id-for-dialogs.js
@@ -0,0 +1,13 @@
+function addIDForDialogs() {
+	const buttonsDialogClose = document.querySelectorAll('.dialog-close');
+
+	buttonsDialogClose.forEach((button) => {
+		const currentSlug = button.getAttribute('slug');
+		const currentDialog = button.closest('[data-name="popup-wrapper"]');
+		const dialogContent = currentDialog.querySelector('[data-name="popup"]');
+		currentDialog.setAttribute('id', currentSlug);
+		dialogContent.setAttribute('aria-labelledby', `dialog-${currentSlug}`);
+	});
+}
+
+addIDForDialogs();
