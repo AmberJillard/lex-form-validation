c0fbc5668b71a94c4cdca2bac9995492617fecad
AmberJillard <amber@lexgillette.com>
2025-08-13 10:02:03 -0700

    Create move-legend.js

---
+++ move-legend.js
@@ -0,0 +1,10 @@
+function moveLegend() {
+	const legends = document.querySelectorAll('legend');
+	legends.forEach((legend) => {
+		const parent = legend.parentNode;
+		parent.before(legend);
+		parent.remove();
+	});
+}
+
+moveLegend();
